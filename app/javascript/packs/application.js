929a8595a8d8ad6a390f88d83f38cb37167f6ef1
leonardo ferreira <leovsferreira@gmail.com>
2019-03-11 17:31:30 -0300

    Fixing app.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,11 @@
 import "bootstrap";
 
+// CSS
+import 'mapbox-gl/dist/mapbox-gl.css';
+// internal imports
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
 
 $(document).ready(function(){
 
