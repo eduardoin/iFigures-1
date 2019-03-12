0a2b502a2387f7b9c9320e75d071c53dc3fbe0ea
leonardo ferreira <leovsferreira@gmail.com>
2019-03-12 12:07:47 -0300

    Added reservation system

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :figures
+  resources :figures do
+    resources :reservations, only: :create
+  end
   get "/profile", to: "profile#show"
   get "/dashboard", to: "dashboard#view"
-  
+
   resources :brands, only: [:show], param: :name, :controller => "pages"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
