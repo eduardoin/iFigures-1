2b6fdb4072ccbcb15ad0734d2415a87758838121
leonardo ferreira <leovsferreira@gmail.com>
2019-03-12 15:08:42 -0300

    Add autocomplete to address

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,9 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 // internal imports
 import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
+initAutocomplete();
 initMapbox();
 
 $(document).ready(function(){
